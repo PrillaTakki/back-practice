f0dc3b2642baf6fddec0c3788f14e0cb405108ea
PrillaTakki <mycryptoan@gmail.com>
2024-01-25 01:43:05 +0400

    add redis

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import { createClient } from 'redis';
 
 dotenv.config();
 
@@ -16,20 +17,36 @@ app.use(
 
 app.use(cors());
 
+const redisClient = createClient();
+const connection = redisClient.connect();
+
 let clicks = 0;
 
 interface ClicksData {
   clicks: number;
 }
 
-app.get('/clicks/', function (request, response) {
+app.get('/clicks/', async function (request, response) {
+  const redis = await connection;
+  const savedClicks = await redis.get('clicks');
+
+  let result = 0;
+
+  if (savedClicks !== null) {
+    result = parseInt(savedClicks);
+  }
+
   const data: ClicksData = {
-    clicks: clicks,
+    clicks: result,
   };
   response.send(data);
 });
 
-app.post('/clicks/', function (request, response) {
+app.get('/', function (request, response) {
+  response.send('Я живой и супер горячий!');
+});
+
+app.post('/clicks/', async function (request, response) {
   if (typeof request.body.clicks !== 'number') {
     response.sendStatus(400);
     return;
@@ -37,9 +54,11 @@ app.post('/clicks/', function (request, response) {
 
   const data: ClicksData = request.body;
   clicks = data.clicks;
+  const redis = await connection;
+  await redis.set('clicks', data.clicks);
 
   const responseData: ClicksData = {
-    clicks: clicks,
+    clicks: data.clicks,
   };
   response.send(responseData);
 });
